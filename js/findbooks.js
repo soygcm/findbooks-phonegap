987fcc57ca3255b19f0c44e70f2a7129bd3a94d5
soygcm <gcmelmejor@gmail.com>
2012-12-10 13:00:02 -0600

    Agregar nueva interfaz
    Solucionar Scroll en Android

--- js/findbooks.js
+++ js/findbooks.js
@@ -1,4 +1,34 @@
+function isTouchDevice(){
+  try{
+    document.createEvent("TouchEvent");
+    $("#highlight").prepend('isTouchDevice')
+    return true;
+  }catch(e){
+    return false;
+  }
+}
+function touchScroll(id){
+  if(isTouchDevice()){ //if touch events exist...
+    var el=document.getElementById(id);
+    var scrollStartPos=0;
+
+    document.getElementById(id).addEventListener("touchstart", function(event) {
+      scrollStartPos=this.scrollTop+event.touches[0].pageY;
+      event.preventDefault();
+    },false);
+
+    document.getElementById(id).addEventListener("touchmove", function(event) {
+      this.scrollTop=scrollStartPos-event.touches[0].pageY;
+      event.preventDefault();
+    },false);
+  }
+}
+
+
+
 $(function() {
+
+  touchScroll("highlight");
 
   Parse.$ = jQuery;
 
