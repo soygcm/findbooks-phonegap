e6ac9824168b611684f28917ff8bd63c394a20a6
soygcm <gcmelmejor@gmail.com>
2013-02-22 10:12:55 -0600

    Comentar AppState Object

--- app/models.js
+++ app/models.js
@@ -1,6 +1,6 @@
 var Book = Parse.Object.extend("Book");
 var User = Parse.Object.extend("User");
 var Offer = Parse.Object.extend("Offer");
-var AppState = Parse.Object.extend("AppState");
+// var AppState = Parse.Object.extend("AppState");
 var BookList = Parse.Collection.extend({model: Book});
 var OfferList = Parse.Collection.extend({model: Offer});
