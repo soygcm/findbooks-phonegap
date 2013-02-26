8a1e2e71fd96c6ef4ffbfcf41c3afee5abb2e31c
soygcm <gcmelmejor@gmail.com>
2013-02-26 08:51:37 -0600

    Agregar un nuevo libro, solo si no se encuentra en la lista

--- app/views/home.js
+++ app/views/home.js
@@ -10,10 +10,6 @@ var HomeView = Parse.View.extend({
     // _.bindAll(this, "logIn", "signUp");
     this.render();
     this.mainView = this.$('#home');
-    this.personalOffers = new OfferList;
-    this.personalOffers.query = new Parse.Query('Offer');
-    this.personalOffers.query.equalTo("user", Parse.User.current());
-    this.personalOffers.bind('add', this.addOne);//? Alguna vez se usa?
     this.getUserBooks();
   },
   render: function() {
@@ -30,25 +26,47 @@ var HomeView = Parse.View.extend({
     this.mainView.removeClass('show-right'); 
   },
   getUserBooks: function(){
-    this.$("#personal>div>ul").empty();
+    // this.$("#personal>div>ul").empty();
     self = this;
-
-    Parse.Cloud.run('getUserBooks', {}, {
-      success: function (result){
-        appView.homeView.addPersonalOffers(result)
-        console.log(result);
+    var user = Parse.User.current();
+    this.userBooks = user.relation('books');
+    this.userBooks = this.userBooks.query().collection();
+    this.userBooks.fetch({
+      success: function(books) {
+        // appView.homeView.userBooks = books;
+        appView.homeView.addUserBooks(books);
+        // list contains the posts that the current user likes.
       }
     });
   },
-  addPersonalOffers: function (offers) {
+  addUserBooks: function (books) {
     self = this;
-    jQuery.each(offers, function(index, offer) {
-      self.addOne(offer);
+    books.each(function(book) {
+      self.addOneBook(book);
     });
   },
-  addOne: function (offer){
-    console.log(this.$);
-    var view = new BookView({model: offer});
+  addNewBook: function (bookNew) {
+    var isNew = 0;
+    var isNewPhoto = 0;
+    self = this;
+    jQuery.each(this.userBooks, function(index, book) {
+      // console.log('bookNew:'+bookNew.get('title')+', book:'+book.get('title'));
+      if(!(bookNew.get('title') == book.get('title') &&  bookNew.get('author') == bookNew.get('author'))){
+        isNew++;
+      }
+      if(bookNew.get('picture').url != book.get('picture').url){
+        isNewPhoto++;
+      }
+    });
+    if(isNew>0){
+      self.addOneBook(bookNew);
+    }
+    console.log('Es nuevo el libro? '+isNew+' Es nueva la foto? '+isNewPhoto);
+
+  },
+  addOneBook: function (book){
+    // console.log(book);
+    var view = new BookView({model: book});
     this.$("#personal>div>ul").prepend(view.render().el);
     // console.log(offer);
   },
